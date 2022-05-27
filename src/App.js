601c16182e18b046631ccaf963a1395570ab2e6f
coldblade2000 <diegogranada7@gmail.com>
2022-05-27 13:30:24 -0500

    Aliste para entrega

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {FormattedMessage, IntlProvider, useIntl} from "react-intl";
 import axios from "axios";
-import Lista from "./Lista";
-import Detalle from "./Detalle";
-import localeEnMessages from "./i18n/en.json"
-import localeEsMessages from "./i18n/es.json"
 import Houses from "./Houses";
 import Rooms from "./Rooms";
 
-function App({locale}) {
+function App({}) {
     const [houses, setHouses] = useState([])
     const [selectedHouse, setSelectedHouse] = useState(null)
 
@@ -26,19 +20,12 @@ function App({locale}) {
     }
 
     useEffect(() => {
-        /*const peliculasURL = (intl.locale.startsWith("en")) ?
-            "https://gist.githubusercontent.com/josejbocanegra/8b436480129d2cb8d81196050d485c56/raw/48cc65480675bf8b144d89ecb8bcd663b05e1db0/data-en.json" :
-            "https://gist.githubusercontent.com/josejbocanegra/f784b189117d214578ac2358eb0a01d7/raw/2b22960c3f203bdf4fac44cc7e3849689218b8c0/data-es.json"
-*/
         fetchInfo().catch((e => console.error(e))
         )
     }, [houses.length, rooms.length])
 
     return (
         <div className="d-flex flex-column w-100">
-            {/* <header className="w-100 p-2">
-                <h1><i className="bi bi-camera-reels-fill"></i><FormattedMessage id="bar-title"/></h1>
-            </header>*/}
             <Houses houses={houses} setSelectedHouse={setSelectedHouse}/>
             {selectedHouse && <Rooms roomsRaw={rooms} selectedHouse={selectedHouse}/>}
         </div>
